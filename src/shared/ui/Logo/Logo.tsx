4648e972cc89bcdb037eac20fd980ba211a93126
Vik63 <sfoto116@yandex.ru>
2024-08-03 21:45:37 +0400

    favicon_7

--- src/shared/ui/Logo/Logo.tsx
+++ src/shared/ui/Logo/Logo.tsx
@@ -1,10 +1,13 @@
 import { memo } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '@/shared/assets/icons/favicon_1.png';
 import { classNames } from '@/shared/lib/classNames/classNames';
 
 import cls from './Logo.module.scss';
 import { getRouteMain } from '@/shared/const/router';
 import { useResize } from '@/shared/lib/hooks/useResize';
+import { Icon } from '../Icon';
+import { HStack } from '../Stack';
 
 interface LogoProps {
    className?: string;
@@ -16,7 +19,10 @@ export const Logo = memo((props: LogoProps) => {
 
    return (
       <Link to={getRouteMain()} className={classNames(cls.logo, { [cls.mobile]: isMobile }, [className])}>
-         ИНЖЕНЕРНЫЙ ЦЕНТР
+         <HStack gap={10}>
+            <Icon src={logo} className={cls.logoImage} />
+            ИНЖЕНЕРНЫЙ ЦЕНТР
+         </HStack>
          <span className={classNames(cls.logoName, { [cls.mobile]: isMobile }, [])}>АКАДЕМИЯ ТЕПЛА</span>
       </Link>
    );
